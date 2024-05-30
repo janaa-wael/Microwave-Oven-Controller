5d8d1418bbf2524debecc479897b889742621a51
janaa-wael <janawael119@gmail.com>
2024-05-30 13:37:38 +0300

    TEST: Incrementing readings on LCD after pushing the built-in button of TIVA C

---
+++ TEST DRIVERS/LCD TEST (INCREMENTING VARIABLES WHEN PRESSING THE SWITCH)/gpio.d
@@ -0,0 +1,5 @@
+.\gpio.o: MCAL\GPIO\GPIO.c
+.\gpio.o: MCAL\GPIO\../../LIB/STD_TYPES.h
+.\gpio.o: MCAL\GPIO\../../LIB/tm4c123gh6pm.h
+.\gpio.o: MCAL\GPIO\../../LIB/BIT_MATH.h
+.\gpio.o: MCAL\GPIO\GPIO.h
---
+++ TEST DRIVERS/LCD TEST (INCREMENTING VARIABLES WHEN PRESSING THE SWITCH)/push_btn.d
@@ -0,0 +1,6 @@
+.\push_btn.o: HAL\PUSH_BTN\PUSH_BTN.c
+.\push_btn.o: HAL\PUSH_BTN\../../LIB/BIT_MATH.h
+.\push_btn.o: HAL\PUSH_BTN\../../LIB/STD_TYPES.h
+.\push_btn.o: HAL\PUSH_BTN\../../MCAL/GPIO/GPIO.h
+.\push_btn.o: HAL\PUSH_BTN\../../MCAL/GPIO/../../LIB/tm4c123gh6pm.h
+.\push_btn.o: HAL\PUSH_BTN\PUSH_BTN.h
